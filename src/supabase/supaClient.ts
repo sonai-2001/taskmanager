96aec7c84df54b78c77ca5b842cca3d10a57c60a
sonai-2001 <sonaibhadra11174@gmail.com>
2025-02-01 13:44:09 +0530

    again back to my initial position

--- src/supabase/supaClient.ts
+++ src/supabase/supaClient.ts
@@ -4,10 +4,10 @@ import { createClient } from "@supabase/supabase-js";
 
 
 
-const projectUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string | undefined;
-// const projectUrl = "https://rrtczjjjshgkrffcyiod.supabase.co";
-const apiKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string | undefined;
-// const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJydGN6ampqc2hna3JmZmN5aW9kIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNzA5NDM2NywiZXhwIjoyMDUyNjcwMzY3fQ.7tVQ0dryGMGTBSAxZa5m7Gm0zl-YEmMrO8vgvd2PBkQ";
+// const projectUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string | undefined;
+const projectUrl = "https://rrtczjjjshgkrffcyiod.supabase.co";
+// const apiKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string | undefined;
+const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJydGN6ampqc2hna3JmZmN5aW9kIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNzA5NDM2NywiZXhwIjoyMDUyNjcwMzY3fQ.7tVQ0dryGMGTBSAxZa5m7Gm0zl-YEmMrO8vgvd2PBkQ";
 
 console.log("projectUrl",projectUrl)
 console.log("projectkey",apiKey)
